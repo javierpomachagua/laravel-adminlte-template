a88fb80753d9f2398ae7104962dacc6748bebded
javierpomachagua <javierpp0303@gmail.com>
2023-03-30 16:13:25 -0500

    add input blade components

--- resources/views/events-create.blade.php
+++ resources/views/events-create.blade.php
@@ -7,5 +7,32 @@
 @stop
 
 @section('content')
+    <div>
+        <div class="row">
+            <x-adminlte-input name="name" label="Nombre" placeholder="Nombre del evento"
+                              fgroup-class="col-md-6"/>
+        </div>
+        <div class="row">
+            <x-adminlte-textarea name="description" label="Descripción" placeholder="Descripción del evento"
+                                 fgroup-class="col-md-6"/>
+        </div>
+        <div class="row">
+            <x-adminlte-select name="type" label="Tipos" fgroup-class="col-md-6">
+                <x-slot name="prependSlot">
+                    <div class="input-group-text bg-gradient-info">
+                        <i class="fas fa-building"></i>
+                    </div>
+                </x-slot>
+                <option>Seleccione</option>
+                <option>Concierto</option>
+                <option>Fútbol</option>
+            </x-adminlte-select>
+        </div>
+        <div class="row">
+            <div class="form-group col-md-6">
+                <x-adminlte-button label="Registrar" theme="primary" icon="fas fa-save"/>
+            </div>
 
+        </div>
+    </div>
 @endsection
